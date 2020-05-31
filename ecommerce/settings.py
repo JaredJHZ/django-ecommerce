cec57ec05734599d6390eaccc393f83ca0f9bf1d
jared josafhat hernandez zuñiga <jaredjhz2597@icloud.com>
2020-05-31 14:02:18 -0500

    v2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,9 +141,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media_root/'
 
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
