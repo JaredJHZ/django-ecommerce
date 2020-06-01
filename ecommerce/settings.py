8fec3f6cfaf60684bda22fca16e231279b50ed2a
jared josafhat hernandez zuñiga <jaredjhz2597@icloud.com>
2020-06-01 14:47:14 -0500

    added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,9 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# Deployment
 
-MEDIA_URL = '/media_root/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -148,11 +150,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # development
 
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
+
+# MEDIA_URL = '/media_root/'
+
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# # MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Authentication 
 AUTHENTICATION_BACKENDS = (
